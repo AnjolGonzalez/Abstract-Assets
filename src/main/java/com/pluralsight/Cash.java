cc02e899475949300387bc34034f556fd4806007
Angel Gonzalez <agonzalez000@appdev.yearup.org>
2023-11-09 11:39:44 -0500

    First Commit

--- src/main/java/com/pluralsight/Cash.java
+++ src/main/java/com/pluralsight/Cash.java
@@ -1,2 +1,10 @@
-package com.pluralsight;public class Cash {
+package com.pluralsight;
+
+public class Cash extends Asset{
+    public Cash(String dateAcquired, String description, double originalCost) {
+        super(dateAcquired, description, originalCost);
+    }
+    public double value() {
+        return originalCost;
+    }
 }
